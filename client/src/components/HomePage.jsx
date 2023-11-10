b7b588da1f829dac6f9cdab3c6920872882ff1cb
Tom Carfano <tomcarfano@TC-Macbook-Pro.lan>
2023-11-09 17:41:06 -0800

    add homepage details

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -35,6 +35,9 @@ const HomePage = () => {
     <h2 className='contactInfo'>Store Hours: {storeHoursToday()}</h2>
     <Link to="tel:9492501900" className='contactInfo'><h2>Phone Number: (949) 250-1900</h2></Link>
     <Link to="mailto:nbgolshop@att.net" className='contactInfo'> <h2>Email: NBGolfShop@att.net</h2></Link>
+    <Link to="https://www.google.com/maps/place/Steve+Carfano's+Newport+Beach+Golf+Shop/@33.6592756,-117.8809357,15z/data=!4m6!3m5!1s0x80dcdfab78b3b055:0xb70abddf8d02c58f!8m2!3d33.6592756!4d-117.8809357!16s%2Fg%2F1hf5c32rz?entry=ttu" className='contactInfo'> <h2>Address: 3100 Irvine Ave. Newport Beach</h2></Link>
+    <br/>
+    <br/>
     <Footer/>
     </>
   )
